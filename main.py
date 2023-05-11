24a0676350f801924403e6d2658725a4892e90d3
maxfuligni <maxfuligni@gmail.com>
2023-05-10 20:58:54 -0400

    more stats

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # This is a sample Python script.
 import openpyxl as openpyxl
 
-
 import pandas as pd
 import pip
 
@@ -10,8 +9,6 @@ import pip
 # Read the .xlsx file
 file_path = "exterminator_data.xlsx"
 df = pd.read_excel(file_path, engine='openpyxl')
-
-
 
 print("Column names:")
 print(df.columns)
@@ -41,7 +38,6 @@ for technician, count in tech_new_regular_accounts.items():
     print(f"{technician}: {count}")
 
 tech_completed_regular_accounts = {}
-
 
 for index, row in df.iterrows():
     technician = row['Tech Assigned']
@@ -288,3 +284,208 @@ for index, row in df.iterrows():
 print("Number of Scheduled Jobs per Tech:")
 for technician, count in tech_scheduled_jobs_count.items():
     print(f"{technician}: {count}")
+
+# Create a dictionary to store the count of proposals per tech
+tech_proposals_count = {}
+
+# Iterate over the rows of the DataFrame
+for index, row in df.iterrows():
+    technician = row['Tech Assigned']
+    lead_status = row['Lead Status']
+
+    # Check if the criteria for a proposal are met
+    if lead_status == '3P - Pending Prop':
+        # Increment the count of proposals for the technician
+        if technician in tech_proposals_count:
+            tech_proposals_count[technician] += 1
+        else:
+            tech_proposals_count[technician] = 1
+
+# Print the count of proposals per tech
+print("Number of Proposals per Tech:")
+for technician, count in tech_proposals_count.items():
+    print(f"{technician}: {count}")
+
+# Create dictionaries to store the count of No Contacts and Passes per tech
+tech_no_contacts_count = {}
+tech_passes_count = {}
+
+# Iterate over the rows of the DataFrame
+for index, row in df.iterrows():
+    technician = row['Tech Assigned']
+    lead_status = row['Lead Status']
+
+    # Check if the criteria for a No Contact or Pass are met
+    if lead_status in ['2-No Contact', '9-Pass']:
+        if lead_status == '2-No Contact':
+            # Increment the count of No Contacts for the technician
+            if technician in tech_no_contacts_count:
+                tech_no_contacts_count[technician] += 1
+            else:
+                tech_no_contacts_count[technician] = 1
+        elif lead_status == '9-Pass':
+            # Increment the count of Passes for the technician
+            if technician in tech_passes_count:
+                tech_passes_count[technician] += 1
+            else:
+                tech_passes_count[technician] = 1
+
+# Print the count of No Contacts and Passes per tech
+print("Number of No Contacts and Passes per Tech:")
+for technician in set(tech_no_contacts_count.keys()).union(tech_passes_count.keys()):
+    no_contacts_count = tech_no_contacts_count.get(technician, 0)
+    passes_count = tech_passes_count.get(technician, 0)
+    print(f"{technician}: {no_contacts_count} No Contacts / {passes_count} Passes")
+
+# Create a dictionary to store the count of other source calls per tech
+tech_other_source_calls_count = {}
+
+# Iterate over the rows of the DataFrame
+for index, row in df.iterrows():
+    technician = row['Tech Assigned']
+    how_heard = row['How did you hear about us']
+
+    # Check if the criteria for an other source call is met
+    if pd.isna(how_heard):
+        # Increment the count of other source calls for the technician
+        if technician in tech_other_source_calls_count:
+            tech_other_source_calls_count[technician] += 1
+        else:
+            tech_other_source_calls_count[technician] = 1
+
+# Print the count of other source calls per tech
+print("Number of Other Source Calls per Tech:")
+for technician, count in tech_other_source_calls_count.items():
+    print(f"{technician}: {count}")
+
+# Create a dictionary to store the count of completed non-recurring, non-zero-charge leads per tech
+tech_completed_non_recurring_non_zero_charge_count = {}
+
+# Iterate over the rows of the DataFrame
+for index, row in df.iterrows():
+    technician = row['Tech Assigned']
+    lead_status = row['Lead Status']
+    service_type = row['Service Type']
+    service_charge = row['Service Charge']
+
+    # Check if the criteria for a completed non-recurring, non-zero-charge lead are met
+    if lead_status == '8-Completed' and service_type != 'Recurring' and service_charge != 0:
+        # Increment the count for the technician
+        if technician in tech_completed_non_recurring_non_zero_charge_count:
+            tech_completed_non_recurring_non_zero_charge_count[technician] += 1
+        else:
+            tech_completed_non_recurring_non_zero_charge_count[technician] = 1
+
+# Create a dictionary to store the number of completed leads per tech
+tech_completed_leads_count = {}
+
+# Iterate over the rows of the DataFrame
+for index, row in df.iterrows():
+    technician = row['Tech Assigned']
+    lead_status = row['Lead Status']
+
+    # Check if the lead status is "8 - Complete"
+    if lead_status == '8-Completed':
+        # Increment the count of completed leads for the technician
+        if technician in tech_completed_leads_count:
+            tech_completed_leads_count[technician] += 1
+        else:
+            tech_completed_leads_count[technician] = 1
+
+
+# Create a dictionary to store the percentage sold per tech
+tech_sold_percentage = {}
+
+# Iterate over the technicians
+for technician in tech_new_calls_count.keys():
+    # Calculate the percentage sold per tech
+   # print("%d %d new calls for %s", tech_new_calls_count[technician], tech_completed_leads_count[technician])
+    tech_sold_percentage[technician] = (tech_new_calls_count[technician] / tech_completed_leads_count[technician]) * 100
+
+# Print the percentage sold per tech
+for tech, percentage in tech_sold_percentage.items():
+    print(f'Technician: {tech}, % sold: {percentage}%')
+
+    # # Calculate the percentage of calls sold per tech
+    # print("Percentage of Calls Sold per Tech:")
+    # for technician in set(tech_new_calls_count.keys()).union(tech_completed_non_recurring_non_zero_charge_count.keys()):
+    #     new_calls_count = tech_new_calls_count.get(technician, 0)
+    #     completed_count = tech_completed_non_recurring_non_zero_charge_count.get(technician, 0)
+    #     if new_calls_count == 0:  # Avoid division by zero
+    #         percentage_sold = 0
+    #     else:
+    #         #percentage_sold = (new_calls_count / completed_count) * 100
+    #         percentage_sold = (completed_count / new_calls_count) * 100
+    #     print(f"{technician}: {percentage_sold:.2f}%")
+
+    # Create a dictionary to store the count of completed jobs per tech and the total service charge collected per tech
+    tech_completed_jobs_count = {}
+    tech_total_service_charge = {}
+
+    # Iterate over the rows of the DataFrame
+    for index, row in df.iterrows():
+        technician = row['Tech Assigned']
+        lead_status = row['Lead Status']
+        service_charge = row['Service Charge']
+
+        # Check if the lead status is "Completed"
+        if lead_status == '8-Completed':
+            # Increment the count of completed jobs for the technician
+            if technician in tech_completed_jobs_count:
+                tech_completed_jobs_count[technician] += 1
+            else:
+                tech_completed_jobs_count[technician] = 1
+
+            # Add the service charge to the total service charge collected by the technician
+            if technician in tech_total_service_charge:
+                tech_total_service_charge[technician] += service_charge
+            else:
+                tech_total_service_charge[technician] = service_charge
+
+    # Print the count of completed jobs per tech and the total service charge collected per tech
+print("Total Number of Jobs Completed per Tech:")
+for technician, count in tech_completed_jobs_count.items():
+    print(f"{technician}: {count}")
+
+print("\nTotal/Gross Amount Collected per Tech:")
+for technician, total in tech_total_service_charge.items():
+    print(f"{technician}: ${total:.2f}")
+
+    # Calculate and print the total number of completed jobs by summing up the counts for each technician
+total_jobs_completed = sum(tech_completed_jobs_count.values())
+print(f"\nTotal Number of Jobs Completed (by all technicians): {total_jobs_completed}")
+
+# Create dictionaries to store the total amount billed/bartered per tech and the count of leads with status "1-Initial" per tech
+tech_billed_amount = {}
+tech_initial_leads_count = {}
+
+# Iterate over the rows of the DataFrame
+for index, row in df.iterrows():
+    technician = row['Tech Assigned']
+    lead_status = row['Lead Status']
+    service_charge = row['Service Charge']
+    payment_method = row['Check, Invoice, or Reference Info']
+
+    # Check if the payment method is "Invoice"
+    if payment_method == 'To be invoiced':
+        # Add the service charge to the total amount billed/bartered by the technician
+        if technician in tech_billed_amount:
+            tech_billed_amount[technician] += service_charge
+        else:
+            tech_billed_amount[technician] = service_charge
+
+    # Check if the lead status is "1-Initial"
+    if lead_status == '1-Initial':
+        # Increment the count of leads with status "1-Initial" for the technician
+        if technician in tech_initial_leads_count:
+            tech_initial_leads_count[technician] += 1
+        else:
+            tech_initial_leads_count[technician] = 1
+
+# Print the total amount billed/bartered per tech
+for tech, amount in tech_billed_amount.items():
+    print(f'Technician: {tech}, Total amount billed/bartered: ${amount}')
+
+# Print the count of leads with status "1-Initial" per tech
+for tech, count in tech_initial_leads_count.items():
+    print(f'Technician: {tech}, # of leads as initials: {count}')
